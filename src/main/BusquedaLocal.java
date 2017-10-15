ee1c7a3cf0ffc456d99d9c1b09f835ee98acee7f
jabm0010 <jabm0010@red.ujaen.es>
2017-10-15 11:45:48 +0200

    Update BusquedaLocal.java

--- src/main/BusquedaLocal.java
+++ src/main/BusquedaLocal.java
@@ -57,8 +57,6 @@ public class BusquedaLocal {
         //Generamos un número aleatorio para calcular el transmisor por el cual vamos a empezar
         int numero = (int) Math.floor(Math.random() * _transmisores.transmisores.size());
 
-        
-       
         for (int i = 0; i < 10000; i++) {
 
             //Genera un random para calcular el sentido en el que nos movemos a la hora de buscar
@@ -67,11 +65,11 @@ public class BusquedaLocal {
             double sentido = Math.random();
 
             int valorInicial = solucion.get(numero);//La frecuencia asociada al transmisor en la solución actual
-            
+
             int indiceInicial = 3; //La inicio a cualquier valor, por ejemplo 3
-            
+
             int frecAsociada = _transmisores.transmisores.get(numero); //El rango de frecuencias a las que puede acceder el transmisor
-            
+
             int nuevoCoste = 99999; //Nuevo coste al cambiar la frecuencia del transmisor. Iniciado por defecto a 99999.
 
             //Busco la posición de la frecuencia que tenemos para el transmisor actual en el total
@@ -86,17 +84,22 @@ public class BusquedaLocal {
             //Recorrido hacia la izquierda
             if (sentido < 0.5) {
                 boolean encontrado = false;
-                
+
                 //El último valor que se puede acceder es 2, ya que 0 es el rango de Frecuencia y 1 el
                 //número de frecuencias existentes en ese rango
-                
                 while (indiceInicial >= 2 && encontrado == false) {
+                    int facto = rDiferenciaFactorizacion(solucion, numero + 1);
+
                     indiceInicial--;
                     valorInicial = _frecuencias.rangoFrecuencias.get(frecAsociada).get(indiceInicial);
                     List<Integer> nuevaSolucion = new ArrayList<>();
                     nuevaSolucion = solucion;
                     nuevaSolucion.set(numero, valorInicial);
-                    nuevoCoste = rDiferencia(nuevaSolucion);
+                    int facto2 = rDiferenciaFactorizacion(nuevaSolucion, numero + 1);
+                    //Factorizo, elimino el coste de las frecuencias del primer valor y 
+                    //sumo el coste de las frecuencias del valor nuevo
+                    nuevoCoste = resultado - facto + facto2;
+
                     if (nuevoCoste < resultado) {
                         solucion = nuevaSolucion;
                         resultado = nuevoCoste;
@@ -108,12 +111,16 @@ public class BusquedaLocal {
                 boolean encontrado = false;
 
                 while (indiceInicial < _frecuencias.rangoFrecuencias.get(frecAsociada).get(1) && encontrado == false) {
+                    int facto = rDiferenciaFactorizacion(solucion, numero + 1);
+
                     indiceInicial++;
                     valorInicial = _frecuencias.rangoFrecuencias.get(frecAsociada).get(indiceInicial);
                     List<Integer> nuevaSolucion = new ArrayList<>();
                     nuevaSolucion = solucion;
                     nuevaSolucion.set(numero, valorInicial);
-                    nuevoCoste = rDiferencia(nuevaSolucion);
+                    int facto2 = rDiferenciaFactorizacion(nuevaSolucion, numero + 1);
+                    nuevoCoste = resultado - facto + facto2;
+
                     if (nuevoCoste < resultado) {
                         solucion = nuevaSolucion;
                         resultado = nuevoCoste;
@@ -122,32 +129,13 @@ public class BusquedaLocal {
 
                 }
             }
-            
-            
-            numero=numero++%_transmisores.transmisores.size();
-            
 
+            numero = numero+1 % _transmisores.transmisores.size();
 
         }
 
     }
 
-    /**
-     * Algoritmo greedy: Asignar un valor al transmisor de forma iterativa e ir
-     * calculando uno por uno. Si el resultado mejora sustituir la lista de
-     * solución
-     *
-     * @param l
-     * @param r
-     */
-    public void algoritmo() throws FileNotFoundException {
-        int menorInterferencia = 999999; //Valor a minimizar. 
-
-        //int result = rDiferencia();
-        //if ( resultado > result )
-        //    resultado = result;
-        //System.out.print(resultado);
-    }
 
     /**
      * Lee el fichero ctr.txt y para sacar las diferencias que se han de
@@ -186,8 +174,38 @@ public class BusquedaLocal {
         return total;
     }
 
-    
-    
-    
-    
+    public int rDiferenciaFactorizacion(List<Integer> _solucion, int tr) throws FileNotFoundException {
+        File fichero = new File("conjuntos/" + main.DIRECTORIO + "/ctr.txt");
+        Scanner lectura = new Scanner(fichero);
+        int total = 0;
+        while (lectura.hasNextLine()) {
+            String linea = lectura.nextLine();
+            if (linea.matches("(.* .* C . .* .)")) {
+                System.out.println(linea);
+                Scanner sLinea = new Scanner(linea);
+                while (sLinea.hasNextInt()) {
+                    int tr1 = sLinea.nextInt();
+                    int tr2 = sLinea.nextInt();
+                    sLinea.next();
+                    sLinea.next();
+                    int diferencia = sLinea.nextInt();
+                    int result = sLinea.nextInt();
+
+                    //Si alguno de los dos transmisores es igual a tr, que es el que le pasamos
+                    //entonces calcula la diferencia
+                    if (tr == tr1 || tr == tr2) {
+                        if (Math.abs(_solucion.get(tr1 - 1) - _solucion.get(tr2 - 1)) > diferencia) {
+                            total += result;
+                        }
+                    }
+                }
+                sLinea.close();
+            }
+        }
+        lectura.close();
+
+        return total;
+    }
+
 }
+
