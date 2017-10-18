c55bb4c579596806098c07f612ead1eb6e98e1a6
jabm0010 <jabm0010@red.ujaen.es>
2017-10-18 18:38:55 +0200

    Add files via upload

---
+++ src/main/BusquedaTabu.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ *
+ * @author Pierre Simon
+ */
+public class BusquedaTabu 
+{
+    List<List<Integer>> frecuencias = new ArrayList<> ();
+    List<Integer> transmisores = new ArrayList<> ();
+    List<Integer> frecuenciasR = new ArrayList<> (); // Cada posicion es la frecuencia asignada a dicho transmisor
+    int resultado;    
+    
+    public BusquedaTabu ( listaTransmisores _transmisores, rangoFrec _frecuencias ) 
+    {
+        frecuencias = _frecuencias.rangoFrecuencias;
+        transmisores = _transmisores.transmisores;
+        Random numero = new Random ();
+        resultado = Integer.MAX_VALUE;
+        for ( int i = 0; i < transmisores.size(); i++ )
+        {
+            frecuenciasR.add(frecuencias.get(transmisores.get(i)).get(numero.nextInt(frecuencias.get(transmisores.get(i)).size())));
+        }
+    }
+    
+    
+    
+}
