8299e86f1d1d249505e7eacccc232420bba95822
jabm0010 <jabm0010@red.ujaen.es>
2017-10-18 18:37:28 +0200

    Mejora optimización, greedy y menu
    Cambios que he hecho:
    -Clase restricciones, para evitar tener que hacer la lectura del archivo en cada ciclo he creado una clase restricción donde se guardan los datos en una List<List<Integer>> , mejorando el tiempo de ejecución del algoritmo
    -Búsqueda local, he hecho los ajustes necesarios, la lógica sigue siendo la misma pero ahora en vez de leer desde el fichero para las restricciones coge los datos directamente
    -Greedy, algoritmo greedy aleatorio, como lo piden como un algoritmo aparte en la práctica lo he montado ya para tenerlo en un .java diferente
    -En el main he añadido un menu para hacer más accesible el uso cuando queramos probar varios algoritmos

--- src/main/BusquedaLocal.java
+++ src/main/BusquedaLocal.java
@@ -8,6 +8,7 @@ package main;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
@@ -18,204 +19,153 @@ import java.util.Scanner;
  */
 public class BusquedaLocal {
 
-    List<List<Integer>> rangoFrecuencias = new ArrayList<>();
+    List<List<Integer>> frecuencias = new ArrayList<>();
     List<Integer> transmisores = new ArrayList<>();
-    List<Integer> solucion = new ArrayList<>();
+    List<Integer> frecuenciasR = new ArrayList<>(); // Cada posicion es la frecuencia asignada a dicho transmisor
+    restricciones rest;
+    int resultado;
 
-    int resultado = Integer.MAX_VALUE;
-
-    public BusquedaLocal(listaTransmisores _transmisores, rangoFrec _frecuencias) throws FileNotFoundException {
-        rangoFrecuencias = _frecuencias.rangoFrecuencias;
+    public BusquedaLocal(listaTransmisores _transmisores, rangoFrec _frecuencias, restricciones _restricciones) {
+        frecuencias = _frecuencias.rangoFrecuencias;
         transmisores = _transmisores.transmisores;
-
-        /*
-        
-        Solucion inicial
-        
-        Recorro la lista de transmisores y le asigno a cada uno una frecuencia aleatoria, que se guarda
-        en List<Intger> solucion.
-        
-        Indice=rangoFrec del transmisor.
-        numFrec= el número de frecuencias disponibles asociadas a ese rango. Viene determinada
-        por el segundo valor en cada fila de dom.txt
-        numero= posicion aleatoria de las que existen en cada fila de dom.txt para asociar una frecuencia
-        frec= frecuencia asignada
-        
-        
-         */
+        rest = _restricciones;
+        Random numero = new Random();
+        resultado = Integer.MAX_VALUE;
         for (int i = 0; i < transmisores.size(); i++) {
-            int indice = transmisores.get(i);
-            int numFrec = rangoFrecuencias.get(indice).get(0);
-            int numero = (int) Math.floor(Math.random() * (numFrec) + 1);
-            numero=numero -1;
-            int frec = rangoFrecuencias.get(indice).get(numero);
-            solucion.add(frec);
+            frecuenciasR.add(frecuencias.get(transmisores.get(i)).get(numero.nextInt(frecuencias.get(transmisores.get(i)).size())));
         }
-
-        resultado = rDiferencia(solucion);
-
-        //Generamos un número aleatorio para calcular el transmisor por el cual vamos a empezar
-        int numero = (int) Math.floor(Math.random() * _transmisores.transmisores.size());
-
-        for (int i = 0; i < 100; i++) {
-            System.out.println("Ciclo "+i);
-
-            //Genera un random para calcular el sentido en el que nos movemos a la hora de buscar
-            //una nueva frecuencia para el transmisor
-            //<0.5 izquierda, >=0.5 derecha
-            double sentido = Math.random();
-
-            int valorInicial = solucion.get(numero);//La frecuencia asociada al transmisor en la solución actual
-
-            int indiceInicial = 3; //La inicio a cualquier valor, por ejemplo 3
-
-            int frecAsociada = _transmisores.transmisores.get(numero); //El rango de frecuencias a las que puede acceder el transmisor
-
-            int nuevoCoste = 99999; //Nuevo coste al cambiar la frecuencia del transmisor. Iniciado por defecto a 99999.
-
-            //Busco la posición de la frecuencia que tenemos para el transmisor actual en el total
-            //de todas las frecuencias que se le podrían asignar
-            for (int j = 0; j < _frecuencias.rangoFrecuencias.get(frecAsociada).size(); j++) {
-                if (valorInicial == _frecuencias.rangoFrecuencias.get(frecAsociada).get(j)) {
-                    indiceInicial = j;
-                }
-
-            }
-
-            //Recorrido hacia la izquierda
-            if (sentido < 0.5) {
-                boolean encontrado = false;
-
-                //El último valor que se puede acceder es 2, ya que 0 es el rango de Frecuencia y 1 el
-                //número de frecuencias existentes en ese rango
-                while (indiceInicial >= 1 && encontrado == false) {
-                    int facto = rDiferenciaFactorizacion(solucion, numero + 1);
-
-                    indiceInicial--;
-                    valorInicial = _frecuencias.rangoFrecuencias.get(frecAsociada).get(indiceInicial);
-                    List<Integer> nuevaSolucion = new ArrayList<>();
-                    nuevaSolucion = solucion;
-                    nuevaSolucion.set(numero, valorInicial);
-                    int facto2 = rDiferenciaFactorizacion(nuevaSolucion, numero + 1);
-                    //Factorizo, elimino el coste de las frecuencias del primer valor y 
-                    //sumo el coste de las frecuencias del valor nuevo
-                    nuevoCoste = resultado - facto + facto2;
-
-                    if (nuevoCoste < resultado) {
-                        solucion = nuevaSolucion;
-                        resultado = nuevoCoste;
-                        encontrado = true;
-                    }
-
-                }
-            } else {
-                boolean encontrado = false;
-
-                while (indiceInicial < _frecuencias.rangoFrecuencias.get(frecAsociada).size()-1 && encontrado == false) {
-                    int facto = rDiferenciaFactorizacion(solucion, numero + 1);
-
-                    indiceInicial++;
-                    /*
-                    if(indiceInicial==_frecuencias.rangoFrecuencias.get(frecAsociada).get(1)){
-                        break;
-                    }
-                    */
-                   
-                    valorInicial = _frecuencias.rangoFrecuencias.get(frecAsociada).get(indiceInicial);
-                    List<Integer> nuevaSolucion = new ArrayList<>();
-                    nuevaSolucion = solucion;
-                    nuevaSolucion.set(numero, valorInicial);
-                    int facto2 = rDiferenciaFactorizacion(nuevaSolucion, numero + 1);
-                    nuevoCoste = resultado - facto + facto2;
-
-                    if (nuevoCoste < resultado) {
-                        solucion = nuevaSolucion;
-                        resultado = nuevoCoste;
-                        encontrado = true;
-                    }
-
-                }
-            }
-
-            numero = numero+1;
-            if(numero==transmisores.size()){
-                numero=0;
-            }
-
-        }
-
     }
-
 
     /**
-     * Lee el fichero ctr.txt y para sacar las diferencias que se han de
-     * realizar
+     * Algoritmo greedy: Asignar un valor al transmisor de forma iterativa e ir
+     * calculando uno por uno. Si el resultado mejora sustituir la lista de
+     * solución
      *
-     * @param _solucion
-     * @param _fichero
+     * @param l
+     * @param r
+     */
+    public void algoritmo() throws FileNotFoundException {
+        if (resultado == Integer.MAX_VALUE) {
+            int result = rDiferencia(frecuenciasR, rest); // Da lugar a la solucion inicial
+            if (resultado > result) {
+                resultado = result;
+            }
+            algoritmo();
+        } else {
+            Random numero = new Random();
+            for (int i = 0; i < 1000; i++) {
+                int token = numero.nextInt(transmisores.size());
+                double sentido = numero.nextDouble();
+                int valorInicial = frecuenciasR.get(token); // Se obtiene la frecuencia del token
+                int indiceInicial;
+                int nuevoCoste = Integer.MAX_VALUE;
+
+                indiceInicial = frecuencias.get(transmisores.get(token)).indexOf(valorInicial); // Mas corto que codigo de abajo
+
+                if (sentido < 0.5) {
+                    boolean encontrado = false;
+                    while (indiceInicial >= 0 && !encontrado) {
+                        int fact1 = rDiferencia(frecuenciasR, token, rest);
+                        valorInicial = frecuencias.get(transmisores.get(token)).get(indiceInicial);
+                        List<Integer> nuevaSolucion = new ArrayList<>();
+                        nuevaSolucion.addAll(frecuenciasR);
+                        nuevaSolucion.set(token, valorInicial);
+                        int fact2 = rDiferencia(nuevaSolucion, token, rest);
+                        nuevoCoste = resultado - fact1 + fact2;
+
+                        if (nuevoCoste < resultado) {
+                            frecuenciasR = nuevaSolucion;
+                            resultado = nuevoCoste;
+                            encontrado = true;
+                        }
+                        indiceInicial--;
+                    }
+                } else {
+                    boolean encontrado = false;
+                    while (indiceInicial < frecuencias.get(transmisores.get(token)).size() && !encontrado) {
+                        int fact1 = rDiferencia(frecuenciasR, token, rest);
+                        valorInicial = frecuencias.get(transmisores.get(token)).get(indiceInicial);
+                        List<Integer> nuevaSolucion = new ArrayList<>();
+                        nuevaSolucion.addAll(frecuenciasR);
+                        nuevaSolucion.set(token, valorInicial);
+                        int fact2 = rDiferencia(nuevaSolucion, token, rest);
+                        nuevoCoste = resultado - fact1 + fact2;
+
+                        if (nuevoCoste < resultado) {
+                            frecuenciasR = nuevaSolucion;
+                            resultado = nuevoCoste;
+                            encontrado = true;
+                        }
+                        indiceInicial++;
+                    }
+                }
+                System.out.println(i + " : Resultado actual: " + resultado);
+                token = (token + 1) % transmisores.size();
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public int rDiferencia(List<Integer> valores, restricciones rest) throws FileNotFoundException {
+
+        int total = 0;
+        for (int i = 0; i < rest.restricciones.size(); i++) {
+            int tr1 = rest.restricciones.get(i).get(0);
+            int tr2 = rest.restricciones.get(i).get(1);
+            int diferencia = rest.restricciones.get(i).get(2);
+            int result = rest.restricciones.get(i).get(3);
+
+            if (Math.abs(valores.get(tr1 - 1) - valores.get(tr2 - 1)) > diferencia) {
+                total += result;
+            }
+
+        }
+
+        return total;
+    }
+
+    /**
+     * Calcula el resultado del problema a minimizar
+     *
+     * @param valores Valores de los transmisores
+     * @param cambioTransmisor Transmisor al que se le aplico un cambio de
+     * frecuencia
+     * @param rest Restricciones a evaluar
+     * @return
      * @throws FileNotFoundException
      */
-    public int rDiferencia(List<Integer> _solucion) throws FileNotFoundException {
-        File fichero = new File("conjuntos/" + main.DIRECTORIO + "/ctr.txt");
-        Scanner lectura = new Scanner(fichero);
-        int total = 0;
-        while (lectura.hasNextLine()) {
-            String linea = lectura.nextLine();
-            if (linea.matches("(.* .* C . .* .)")) {
-                //System.out.println(linea);
-                Scanner sLinea = new Scanner(linea);
-                while (sLinea.hasNextInt()) {
-                    int tr1 = sLinea.nextInt();
-                    int tr2 = sLinea.nextInt();
-                    sLinea.next();
-                    sLinea.next();
-                    int diferencia = sLinea.nextInt();
-                    int result = sLinea.nextInt();
+    public int rDiferencia(List<Integer> valores, int cambioTransmisor, restricciones rest) throws FileNotFoundException {
 
-                    if (Math.abs(_solucion.get(tr1 - 1) - _solucion.get(tr2 - 1)) > diferencia) {
-                        total += result;
-                    }
+        int total = 0;
+        for (int i = 0; i < rest.restricciones.size(); i++) {
+
+            int tr1 = rest.restricciones.get(i).get(0);
+            int tr2 = rest.restricciones.get(i).get(1);
+
+            if (tr1 == cambioTransmisor || tr2 == cambioTransmisor) {
+                int diferencia = rest.restricciones.get(i).get(2);
+                int result = rest.restricciones.get(i).get(3);
+
+                if (Math.abs(valores.get(tr1 - 1) - valores.get(tr2 - 1)) > diferencia) {
+                    total += result;
                 }
-                sLinea.close();
+
             }
+
         }
-        lectura.close();
 
         return total;
+
     }
 
-    public int rDiferenciaFactorizacion(List<Integer> _solucion, int tr) throws FileNotFoundException {
-        File fichero = new File("conjuntos/" + main.DIRECTORIO + "/ctr.txt");
-        Scanner lectura = new Scanner(fichero);
-        int total = 0;
-        while (lectura.hasNextLine()) {
-            String linea = lectura.nextLine();
-            if (linea.matches("(.* .* C . .* .)")) {
-                //System.out.println(linea);
-                Scanner sLinea = new Scanner(linea);
-                while (sLinea.hasNextInt()) {
-                    int tr1 = sLinea.nextInt();
-                    int tr2 = sLinea.nextInt();
-                    sLinea.next();
-                    sLinea.next();
-                    int diferencia = sLinea.nextInt();
-                    int result = sLinea.nextInt();
-
-                    //Si alguno de los dos transmisores es igual a tr, que es el que le pasamos
-                    //entonces calcula la diferencia
-                    if (tr == tr1 || tr == tr2) {
-                        if (Math.abs(_solucion.get(tr1 - 1) - _solucion.get(tr2 - 1)) > diferencia) {
-                            total += result;
-                        }
-                    }
-                }
-                sLinea.close();
-            }
+    public void resultados() {
+        System.out.println("Coste: " + resultado);
+        for (int i = 0; i < transmisores.size(); i++) {
+            System.out.println("Transmisor " + (i + 1) + ": " + frecuenciasR.get(i));
         }
-        lectura.close();
-
-        return total;
     }
 
+    
 }
-
---
+++ src/main/Greedy.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ *
+ * @author juan
+ */
+public class Greedy {
+
+    List<List<Integer>> frecuencias = new ArrayList<>();
+    List<Integer> transmisores = new ArrayList<>();
+    List<Integer> frecuenciasR = new ArrayList<>(); // Cada posicion es la frecuencia asignada a dicho transmisor
+    restricciones rest;
+    int resultado;
+
+    public Greedy(listaTransmisores _transmisores, rangoFrec _frecuencias, restricciones _restricciones)throws FileNotFoundException {
+        frecuencias = _frecuencias.rangoFrecuencias;
+        transmisores = _transmisores.transmisores;
+        rest = _restricciones;
+        Random numero = new Random();
+
+        for (int i = 0; i < transmisores.size(); i++) {
+            frecuenciasR.add(frecuencias.get(transmisores.get(i)).get(numero.nextInt(frecuencias.get(transmisores.get(i)).size())));
+        }
+
+        resultado = rDiferencia(frecuenciasR, rest);
+    }
+
+    public int rDiferencia(List<Integer> valores, restricciones rest) throws FileNotFoundException {
+
+        int total = 0;
+        for (int i = 0; i < rest.restricciones.size(); i++) {
+            int tr1 = rest.restricciones.get(i).get(0);
+            int tr2 = rest.restricciones.get(i).get(1);
+            int diferencia = rest.restricciones.get(i).get(2);
+            int result = rest.restricciones.get(i).get(3);
+
+            if (Math.abs(valores.get(tr1 - 1) - valores.get(tr2 - 1)) > diferencia) {
+                total += result;
+            }
+
+        }
+
+        return total;
+    }
+    
+    public void resultados(){
+        System.out.println("Coste: "+resultado);
+        for(int i=0;i<transmisores.size();i++){
+            System.out.println("Transmisor "+(i+1)+": "+frecuenciasR.get(i));
+        }
+    }
+
+}
