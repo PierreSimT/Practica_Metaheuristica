b6f11287a5c4a5125763b9e75daa1ba88d56e657
jabm0010 <jabm0010@red.ujaen.es>
2017-10-27 09:58:07 +0200

    Add files via upload
    búsquedaTabú y un main para probarla.
    He subido tambíen un documento de texto explicando un poco lo que he hecho en la busquedaTabu.
    
    Hay que arreglar en la búsqueda tabú errores de out of bound

---
+++ src/main/busquedaTabu2.java
@@ -0,0 +1,302 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author pierrest
+ */
+public class busquedaTabu2 {
+
+    List<List<Integer>> frecuencias = new ArrayList<>();
+    List<Integer> transmisores = new ArrayList<>();
+    List<Integer> frecuenciasR = new ArrayList<>(); // Cada posicion es la frecuencia asignada a dicho transmisor
+    restricciones rest;
+    int resultado;
+
+    //Variables de control para la reinicialización en caso de estancamiento
+    int contadorEstancamiento = 0;
+    int costeCicloPrevio = Integer.MAX_VALUE;
+
+    //BT
+    List<List<Integer>> listaTabu = new ArrayList<>(); //Lista Tabu
+    int tamaFisicolt;                                //Tamaño fisico de la listaTabu
+    int tamalogicolt = 0;                              //Tamaño logico de la listaTabu
+    int indicelt = 0;                                   
+    List<List<Integer>> matrizFrecuencias = new ArrayList<>();
+
+    public busquedaTabu2(listaTransmisores _transmisores, rangoFrec _frecuencias, restricciones _restricciones) {
+        frecuencias = _frecuencias.rangoFrecuencias;
+        transmisores = _transmisores.transmisores;
+        rest = _restricciones;
+
+        tamaFisicolt = (int) transmisores.size() / 6;
+
+        Random numero = new Random();
+        resultado = Integer.MAX_VALUE;
+        for (int i = 0; i < transmisores.size(); i++) {
+            frecuenciasR.add(frecuencias.get(transmisores.get(i)).get(numero.nextInt(frecuencias.get(transmisores.get(i)).size())));
+        }
+        
+        
+    }
+
+    /**
+     * Algoritmo greedy: Asignar un valor al transmisor de forma iterativa e ir
+     * calculando uno por uno. Si el resultado mejora sustituir la lista de
+     * solución
+     *
+     * @param l
+     * @param r
+     */
+    public void algoritmo() throws FileNotFoundException {
+        if (resultado == Integer.MAX_VALUE) {
+            int result = rDiferencia(frecuenciasR, rest); // Da lugar a la solucion inicial
+            if (resultado > result) {
+                resultado = result;
+            }
+            algoritmo();
+        } else {
+            Random numero = new Random();
+            for (int i = 0; i < 10000; i++) {
+
+                boolean tabuActualizado = false;
+                int token = numero.nextInt(transmisores.size());
+                double sentido = numero.nextDouble();
+                int valorInicial = frecuenciasR.get(token); // Se obtiene la frecuencia del token
+                int indiceInicial;
+                int nuevoCoste = Integer.MAX_VALUE;
+
+                indiceInicial = frecuencias.get(transmisores.get(token)).indexOf(valorInicial); // Mas corto que codigo de abajo
+
+                if (sentido < 0.5) {
+                    int contador = 0;
+                    int indiceFinal = indiceInicial + 1;
+                    while (indiceInicial != indiceFinal && contador < 20) {
+                        int fact1 = rDiferencia(frecuenciasR, token, rest);
+                        valorInicial = frecuencias.get(transmisores.get(token)).get(indiceInicial);
+                        boolean esTabu = comprobarTabu(token, valorInicial);
+
+                        if (esTabu = false) {
+                            List<Integer> nuevaSolucion = new ArrayList<>();
+                            nuevaSolucion.addAll(frecuenciasR);
+                            nuevaSolucion.set(token, valorInicial);
+                            int fact2 = rDiferencia(nuevaSolucion, token, rest);
+                            nuevoCoste = resultado - fact1 + fact2;
+
+                            if (nuevoCoste < resultado) {
+                                frecuenciasR = nuevaSolucion;
+                                resultado = nuevoCoste;
+                                insertarTabu(token, valorInicial);
+                                tabuActualizado = true;
+
+                            }
+                        }
+
+                        contador++;
+                        indiceInicial--;
+                        if (indiceInicial == 0) {
+                            indiceInicial = frecuencias.get(transmisores.get(token)).size() - 1;
+                        }
+                    }
+                } else {
+                    int contador = 0;
+                    int indiceFinal = indiceInicial - 1;
+
+                    while (indiceInicial != indiceFinal && contador < 20) {
+                        int fact1 = rDiferencia(frecuenciasR, token, rest);
+                        valorInicial = frecuencias.get(transmisores.get(token)).get(indiceInicial);
+                        boolean esTabu = comprobarTabu(token, valorInicial);
+
+                        if (esTabu == false) {
+                            List<Integer> nuevaSolucion = new ArrayList<>();
+                            nuevaSolucion.addAll(frecuenciasR);
+                            nuevaSolucion.set(token, valorInicial);
+                            int fact2 = rDiferencia(nuevaSolucion, token, rest);
+                            nuevoCoste = resultado - fact1 + fact2;
+
+                            if (nuevoCoste < resultado) {
+                                frecuenciasR = nuevaSolucion;
+                                resultado = nuevoCoste;
+                                insertarTabu(token, valorInicial);
+                                tabuActualizado = true;
+
+                            }
+                        }
+                        indiceInicial++;
+                        contador++;
+                        if (indiceInicial == frecuencias.get(transmisores.get(token)).size() - 1) {
+                            indiceInicial = 1;
+                        }
+                    }
+                }
+                System.out.println(i + " : Resultado actual: " + resultado);
+
+                /*Añadir a matriz de frecuencias*/
+                boolean encontrado = false;
+                for (int j = 0; j < matrizFrecuencias.size(); j++) {
+
+                    //Si ya existe la combinación transmisor-frecuencia en la matriz, aumentar el número de frecuencias
+                    if (matrizFrecuencias.get(j).get(0) == token && matrizFrecuencias.get(j).get(1) == valorInicial) {
+                        int var = matrizFrecuencias.get(j).get(2);
+                        matrizFrecuencias.get(j).set(2, var);
+                        encontrado = true;
+                    }
+                }
+
+                if (encontrado == false) {
+                    List<Integer> l1 = new ArrayList<>();
+                    l1.add(0, token);
+                    l1.add(1, valorInicial);
+                    l1.add(2, 1);
+                    matrizFrecuencias.add(l1);
+                }
+
+                if (costeCicloPrevio == resultado) {
+                    contadorEstancamiento++;
+                } else {
+                    contadorEstancamiento = 0;
+                }
+                costeCicloPrevio = resultado;
+
+                //Comprobar estancamiento
+                if (contadorEstancamiento == 2000) {
+                    pasarMatrizFrecuencias();
+
+                }
+
+                //Cambiar indice lista Tabu si se ha añadido algun valor
+                if (tabuActualizado == true) {
+                    tamalogicolt++;
+                    indicelt++;
+                    //Si tamalogico y tamafisico iguales, poner el tamalogico a 0 para que sobreescriba
+                    //el valor más antiguo
+                    if (indicelt == tamaFisicolt) {
+                        indicelt=0;
+                    }
+                }
+
+                token = (token + 1) % transmisores.size();
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public int rDiferencia(List<Integer> valores, restricciones rest) throws FileNotFoundException {
+
+        int total = 0;
+        for (int i = 0; i < rest.restricciones.size(); i++) {
+            int tr1 = rest.restricciones.get(i).get(0);
+            int tr2 = rest.restricciones.get(i).get(1);
+            int diferencia = rest.restricciones.get(i).get(2);
+            int result = rest.restricciones.get(i).get(3);
+
+            if (Math.abs(valores.get(tr1 - 1) - valores.get(tr2 - 1)) > diferencia) {
+                total += result;
+            }
+
+        }
+
+        return total;
+    }
+
+    /**
+     * Calcula el resultado del problema a minimizar
+     *
+     * @param valores Valores de los transmisores
+     * @param cambioTransmisor Transmisor al que se le aplico un cambio de
+     * frecuencia
+     * @param rest Restricciones a evaluar
+     * @return
+     * @throws FileNotFoundException
+     */
+    public int rDiferencia(List<Integer> valores, int cambioTransmisor, restricciones rest) throws FileNotFoundException {
+
+        int total = 0;
+        for (int i = 0; i < rest.restricciones.size(); i++) {
+
+            int tr1 = rest.restricciones.get(i).get(0);
+            int tr2 = rest.restricciones.get(i).get(1);
+
+            if (tr1 == cambioTransmisor || tr2 == cambioTransmisor) {
+                int diferencia = rest.restricciones.get(i).get(2);
+                int result = rest.restricciones.get(i).get(3);
+
+                if (Math.abs(valores.get(tr1 - 1) - valores.get(tr2 - 1)) > diferencia) {
+                    total += result;
+                }
+
+            }
+
+        }
+
+        return total;
+
+    }
+
+    /*
+   Método para crear una nueva solución en caso de estancamiento
+   A cada transmisor le asocio la frecuencia registrada en matrizFrecuencias que tenga el mayor
+   número de repeticiones
+     */
+    public void pasarMatrizFrecuencias() {
+        List<Integer> nFrecR = new ArrayList<>();
+        int frec;
+        int numRepeticiones;
+        for (int i = 0; i < transmisores.size(); i++) {
+            frec = 0;
+            numRepeticiones = 0;
+            for (int j = 0; j < matrizFrecuencias.size(); j++) {
+                if (matrizFrecuencias.get(j).get(0) == i && matrizFrecuencias.get(j).get(2) > numRepeticiones) {
+                    frec = matrizFrecuencias.get(j).get(1);
+                    numRepeticiones = matrizFrecuencias.get(j).get(2);
+                }
+            }
+            nFrecR.add(i, frec);
+        }
+
+        frecuenciasR = nFrecR;
+
+    }
+
+    public void insertarTabu(int transmisor, int frecuencia) {
+        List<Integer> tabu = new ArrayList<>();
+        tabu.add(0, transmisor);
+        tabu.add(1, frecuencia);
+
+        listaTabu.add(indicelt, tabu);
+
+    }
+
+    public boolean comprobarTabu(int transmisor, int frecuencia) {
+        boolean solucion = false;
+        for (int i = 0; i < tamalogicolt; i++) {
+            if (listaTabu.get(i).get(0) == transmisor && listaTabu.get(i).get(1) == frecuencia) {
+                solucion = true;
+            }
+        }
+
+        return solucion;
+    }
+
+    public void resultados() {
+        System.out.println("Coste: " + resultado);
+        for (int i = 0; i < transmisores.size(); i++) {
+            System.out.println("Transmisor " + (i + 1) + ": " + frecuenciasR.get(i));
+        }
+    }
+
+}
--- src/main/main.java
+++ src/main/main.java
@@ -15,7 +15,7 @@ import java.util.Scanner;
  * @author alumno
  */
 public class main {
-
+    
     public static String DIRECTORIO;
 
     //Variables para el menu
@@ -26,68 +26,85 @@ public class main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws FileNotFoundException {
-
+        
         System.out.println("Directorio donde se encuentran los archivos: ");
         Scanner reader = new Scanner(System.in);
         DIRECTORIO = reader.next();
 
         rangoFrec frecuencias = new rangoFrec();
         listaTransmisores transmisores = new listaTransmisores();
-        restricciones rest = new restricciones();
+        restricciones rest = new restricciones();   
+        
+        busquedaTabu2 bt2=new busquedaTabu2(transmisores, frecuencias, rest);
+        bt2.algoritmo();
+        bt2.resultados();
+        
+        
 
-        while (select != 0) {
-
-            try {
-                System.out.println("Elige opción:\n1.- Greedy"
-                        + "\n2.- Búsqueda Local\n"
-                        + "3.- Búsqueda Tabúr\n"
-                        + "4.- GRASP\n"
-                        + "5.- Cambiar directorio\n"
-                        + "0.- Salir");
-
-                select = Integer.parseInt(scanner.nextLine());
-
-                switch (select) {
-                    case 1:
-                        Greedy greedy = new Greedy(transmisores, frecuencias, rest);
-                        greedy.resultados();
-                        break;
-                    case 2:
-                        BusquedaLocal busquedaLocal = new BusquedaLocal(transmisores, frecuencias, rest);
-                        busquedaLocal.algoritmo();
-                        busquedaLocal.resultados();
-                        
-                        break;
-                    case 3:
-
-                        break;
-                    case 4:
-
-                        break;
-                    case 5:
-
-                        System.out.println("Directorio donde se encuentran los archivos: ");
-
-                        DIRECTORIO = reader.next();
-
-                        frecuencias = new rangoFrec();
-                        transmisores = new listaTransmisores();
-                        rest = new restricciones();
-                    case 0:
-                        System.out.println("Fin");
-                        break;
-                    default:
-                        System.out.println("Número no reconocido");
-                        break;
-                }
-
-                System.out.println("\n"); //Mostrar un salto de línea en Java
-
-            } catch (Exception e) {
-                System.out.println("Uoop! Error!");
-            }
-        }
-
+//        System.out.println("Directorio donde se encuentran los archivos: ");
+//        Scanner reader = new Scanner(System.in);
+//        DIRECTORIO = reader.next();
+//
+//        rangoFrec frecuencias = new rangoFrec();
+//        listaTransmisores transmisores = new listaTransmisores();
+//        restricciones rest = new restricciones();
+//
+//        while (select != 0) {
+//
+//            try {
+//                System.out.println("Elige opción:\n1.- Greedy"
+//                        + "\n2.- Búsqueda Local\n"
+//                        + "3.- Búsqueda Tabú\n"
+//                        + "4.- GRASP\n"
+//                        + "5.- Cambiar directorio\n"
+//                        + "0.- Salir");
+//
+//                select = Integer.parseInt(scanner.nextLine());
+//
+//                switch (select) {
+//                    case 1:
+//                        Greedy greedy = new Greedy(transmisores, frecuencias, rest);
+//                        greedy.resultados();
+//                        break;
+//                    case 2:
+//                        BusquedaLocal busquedaLocal = new BusquedaLocal(transmisores, frecuencias, rest);
+//                        busquedaLocal.algoritmo();
+//                        busquedaLocal.resultados();
+//                        
+//                        break;
+//                    case 3:
+//                        BusquedaTabu busquedaTabu = new BusquedaTabu(transmisores, frecuencias, rest);
+//                        busquedaTabu.algoritmo();
+//                        busquedaTabu.resultados();
+//
+//                        break;
+//                    case 4:
+//
+//                        break;
+//                    case 5:
+//
+//                        System.out.println("Directorio donde se encuentran los archivos: ");
+//
+//                        DIRECTORIO = reader.next();
+//
+//                        frecuencias = new rangoFrec();
+//                        transmisores = new listaTransmisores();
+//                        rest = new restricciones();
+//                    case 0:
+//                        System.out.println("Fin");
+//                        break;
+//                    default:
+//                        System.out.println("Número no reconocido");
+//                        break;
+//                }
+//
+//                System.out.println("\n"); //Mostrar un salto de línea en Java
+//
+//            } catch (Exception e) {
+//                System.out.println("Uoop! Error!");
+//            }
+//        }
+        /*
         BusquedaLocal busquedaLocal = new BusquedaLocal(transmisores, frecuencias, rest);
 
         busquedaLocal.algoritmo();
@@ -97,7 +114,7 @@ public class main {
         for (int i = 0; i < busquedaLocal.frecuenciasR.size(); i++) {
             System.out.println("Transmisor " + (i + 1) + ": " + busquedaLocal.frecuenciasR.get(i));
         }
-
+*/
 //        for ( int x = 0; x < nuevo.rangoFrecuencias.size(); x++ )
 //        {
 //            System.out.print("[");
@@ -109,5 +126,6 @@ public class main {
 //            System.out.println();
 //        }
     }
+    
 
 }
